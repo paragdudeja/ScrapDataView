6608b24fa39c52e66c55c0212c7d1d5c292fa895
Parag <parag1907@gmail.com>
2021-04-17 15:07:32 +0530

    Added requirements.txt

--- DataView/urls.py
+++ DataView/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
 
 admin.site.site_header = "Data View Admin"
 admin.site.site_title = "Data View Admin Portal"
@@ -23,6 +26,8 @@ admin.site.index_title = "Welcome to Data View Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("Home.urls")),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn DataView.wsgi --log-file -
